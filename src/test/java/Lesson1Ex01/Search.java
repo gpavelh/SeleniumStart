7cb9539df032d50604e61559837ede47ee13e8fd
Pavel <gh7777@yandex.ru>
2020-03-11 20:19:58 +0300

    soon

---
+++ src/test/java/Lesson1Ex01/Search.java
@@ -0,0 +1,39 @@
+package Lesson1Ex01;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class Search {
+    private WebDriver webDriver;
+
+    @BeforeMethod
+    public void initDriver() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        webDriver = new ChromeDriver();
+    }
+
+    @Test
+    void YandexSearch() throws InterruptedException {
+        webDriver.get("https://ya.ru");
+        WebElement input = webDriver.findElement(By.id("text"));
+        input.sendKeys("Руддщ цщкдв!");
+        WebElement button = webDriver.findElement(By.xpath("//button[.=\"Найти\"]"));
+        button.click();
+        Thread.sleep(5000);
+        input = webDriver.findElement(By.name("text"));
+        Assert.assertEquals(input.getAttribute("value"),"Hello world!");
+    }
+
+    @AfterMethod
+    public void closeDriver() {
+        webDriver.quit();
+    }
+}
