7a9408ba0cdc0ff0c79f975fbc6bc313f5f926f2
gh <gh7777@mail.ru>
2020-03-21 23:32:08 +0300

    Ex6 fix

--- src/test/java/Lesson6Ex01/TestPage.java
+++ src/test/java/Lesson6Ex01/TestPage.java
@@ -2,7 +2,6 @@ package Lesson6Ex01;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -20,7 +19,7 @@ public class TestPage {
     }
 
     @Test
-    void firstTest() throws InterruptedException {
+    void firstTest() {
         //Login page
         Login login = new Login(webDriver);
         if (login.getLoginName().equals("Login")) {
